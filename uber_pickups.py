a3a643be6dd650592815e568dde4a81cceda7b58
arajeshh <ananyarajesh@gmail.com>
2026-05-03 16:46:10 -0700

    Fix typo in Streamlit title function

--- uber_pickups.py
+++ uber_pickups.py
@@ -5,7 +5,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.titlest.title("Ananya's App: Uber pickups in NYC")
+st.title("Ananya's App: Uber pickups in NYC")
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
